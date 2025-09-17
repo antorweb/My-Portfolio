a6a222db740ba4e90373dc3ddcf105370a499955
Antor Web-Dev <antorremeni@gmail.com>
2025-09-17 14:00:55 +0600

    chnage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -104,8 +104,7 @@
 
 
 import React, { useEffect, useRef } from "react";
-import banner3 from "../assets/images/banner3.png";
-import banner2 from "../assets/images/banner2.png";
+import banner2 from "../assets/images/MyImages.png";
 import {
   FaInstagram,
   FaLinkedinIn,
@@ -117,10 +116,10 @@ const Banner = () => {
   return (
     <section
       id="banner"
-      className="relative overflow-hidden pt-[100px] md:pt-[120px] lg:pt-[150px] min-h-screen bg-[#101010]"
+      className="relative overflow-hidden pt-[50px] lg:pt-[150px] min-h-screen bg-[#101010]"
     >
       {/* ----------- icons */}
-      <div className="flex lg:flex-col absolute top-[90%] right-4 lg:top-[65%] lg:right-18  gap-4 z-10">
+      <div className="flex lg:flex-col absolute top-[92%] right-4 lg:top-[65%] lg:right-18  gap-4 z-10">
         <a
           className="w-8 h-8 bg-black text-white flex justify-center items-center rounded-full hover:bg-[#A53DFF] transition-colors"
           href="https://www.facebook.com/antor.web.dev"
@@ -151,11 +150,11 @@ const Banner = () => {
         </a>
       </div>
       <div className="container">
-        <div id="banner-row" className="flex flex-col-reverse lg:flex-row justify-around items-center px-4 md:px-[20px] lg:px-0 gap-8">
+        <div id="banner-row" className="flex flex-col-reverse lg:flex-row justify-evenly items-center  px-4 gap-[30px] lg:gap-0">
           {/* Mobile Image */}
-          <div className="w-[180px] h-[180px] lg:hidden  relative rounded-full bg-[#ffffff51] overflow-hidden mb-6 md:mb-0">
+          <div className="w-[300px] h-[300px] lg:hidden  relative rounded-full bg-[#ffffff51] overflow-hidden mb-6 md:mb-0">
             <img
-              className="absolute top-[-60px] xs:top-[-70px] sm:top-[-80px] md:top-[-86px] left-[18px] xs:left-[28px] sm:left-[34px] md:left-[38px] w-[90%]"
+              className="absolute top-[-86px] left-[30px]  w-[100%]"
               src={banner2}
               alt="Antor Karmaker portrait"
             />
@@ -164,15 +163,13 @@ const Banner = () => {
           <div className="flex flex-col pt-6 md:pt-[50px] items-center lg:items-start text-center lg:text-left w-full max-w-xl">
             <p className="text-[16px] md:text-[18px] font-medium font-main text-[#ffffff93]">Hi, I am</p>
             <div>
-              <h1 className="type text-[22px] sm:text-[26px] md:text-[32px] lg:text-[40px] font-bold">Antor Karmoker</h1>
+              <h1 className="type text-[22px] sm:text-[26px] md:text-[32px] lg:text-[60px] font-bold">Antor Karmoker</h1>
             </div>
             {/* ----------- card loading */}
-            <div className="card mt-2 md:mt-4">
+            <div className="card ">
               <div className="loader flex flex-col md:flex-row items-center md:items-end">
-                <p className="text-[14px] md:text-[18px] lg:text-[32px] font-normal font-main text-[#f0f0f0] mb-1 md:mb-0 md:mr-2">
-                  I'm a
-                </p>
-                <div className="words">
+                <p className="text-[14px] md:text-[18px] lg:text-[32px] font-normal font-main text-[#f0f0f0] ">    I'm a  </p>
+                <div className="words pb-[12px] h-[40px]">
                   <span className="word">React Developer</span>
                   <span className="word">Frontend Developer</span>
                   <span className="word">jr. Frontend Developer</span>
@@ -185,7 +182,7 @@ const Banner = () => {
           {/* Right Image */}
           <div className="hidden lg:block lg:w-[350px] lg:h-[350px] xl:w-[450px] xl:h-[450px] relative rounded-full bg-[#ffffff51] overflow-hidden">
             <img
-              className="absolute top-[-70px] xl:top-[-86px] left-[28px] xl:left-[38px] w-[90%]"
+              className="absolute top-[-129px] left-[45px] w-[100%]"
               src={banner2}
               alt="Antor Karmaker portrait"
             />
