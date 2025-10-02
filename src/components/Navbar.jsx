f125dd20ad89e54676e9bf2f1648fe89ff1af434
Antor Web-Dev <antorremeni@gmail.com>
2025-10-02 11:16:21 +0600

    chnage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,105 +7,121 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav id="navbar" className="bg-[#101010] sticky top-0 z-50">
-      <div className="container">
-        <div id="navbar-row" className="flex items-center px-[20px] lg:px-0  justify-between">
-          {/*----------- main -Logo */}
-          <Link to="/">
-            <img className="w-24 h-auto" src={logo} alt="my-logo" />
-          </Link>
-          {/*------------- menu-items */}
-          <div>
-            {/* Hamburger Icon for Mobile */}
-            <div className="md:hidden  flex items-center">
-              <button
-                className="text-white focus:outline-none"
-                onClick={() => setIsOpen(!isOpen)}
-                aria-label="Toggle Menu"
-              >
-                <svg
-                  className="w-8 h-8"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="cursor-target">
-              {/* Desktop Menu */}
-              <ul className="hidden md:flex gap-8  items-center">
-                <li>
-                  <Link
-                    to="/about"
-                    className="text-[20px] font-normal font-main text-[#E6E6E6]"
-                  >
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/portfolio"
-                    className="text-[20px] font-normal font-main text-[#E6E6E6]"
-                  >
-                    Portfolio
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/contact"
-                    className="text-[20px] font-normal font-main text-[#E6E6E6]"
-                  >
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-              {/* Mobile Menu */}
-              <ul
-                className={`${
-                  isOpen ? "flex" : "hidden"
-                } flex-col gap-6 items-start absolute top-20 right-4 bg-[#101010] p-6 rounded-lg shadow-lg md:hidden transition-all duration-300`}
-              >
-                <li>
-                  <Link
-                    to="/about"
-                    className="text-[15px] font-normal font-main text-[#E6E6E6]"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/portfolio"
-                    className="text-[15px] font-normal font-main text-[#E6E6E6]"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    Portfolio
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/contact"
-                    className="text-[15px] font-normal font-main text-[#E6E6E6]"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <>
+
+
+<header>
+  <nav id="desktop-nav">
+    <ul>
+      <li class="active"><a href="#home" class="page-scroll">Home</a></li>
+      <li><a href="#about" class="page-scroll">About Us</a></li>
+      <li><a href="#portfolio" class="page-scroll">Showcase</a></li>
+      <li><a href="#contact" class="page-scroll">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
+    
+    
+    </>
+    // <nav id="navbar" className="bg-[#101010] sticky top-0 z-50">
+    //   <div className="container">
+    //     <div id="navbar-row" className="flex items-center px-[20px] lg:px-0  justify-between">
+        
+    //       <Link to="/">
+    //         <img className="w-24 h-auto" src={logo} alt="my-logo" />
+    //       </Link>
+   
+    //       <div>
+         
+    //         <div className="md:hidden  flex items-center">
+    //           <button
+    //             className="text-white focus:outline-none"
+    //             onClick={() => setIsOpen(!isOpen)}
+    //             aria-label="Toggle Menu"
+    //           >
+    //             <svg
+    //               className="w-8 h-8"
+    //               fill="none"
+    //               stroke="currentColor"
+    //               viewBox="0 0 24 24"
+    //               xmlns="http://www.w3.org/2000/svg"
+    //             >
+    //               <path
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //                 strokeWidth={2}
+    //                 d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+    //               />
+    //             </svg>
+    //           </button>
+    //         </div>
+    //         <div className="cursor-target">
+    //           {/* Desktop Menu */}
+    //           <ul className="hidden md:flex gap-8  items-center">
+    //             <li>
+    //               <Link
+    //                 to="/about"
+    //                 className="text-[20px] font-normal font-main text-[#E6E6E6]"
+    //               >
+    //                 About
+    //               </Link>
+    //             </li>
+    //             <li>
+    //               <Link
+    //                 to="/portfolio"
+    //                 className="text-[20px] font-normal font-main text-[#E6E6E6]"
+    //               >
+    //                 Portfolio
+    //               </Link>
+    //             </li>
+    //             <li>
+    //               <Link
+    //                 to="/contact"
+    //                 className="text-[20px] font-normal font-main text-[#E6E6E6]"
+    //               >
+    //                 Contact
+    //               </Link>
+    //             </li>
+    //           </ul>
+    //           {/* Mobile Menu */}
+    //           <ul
+    //             className={`${
+    //               isOpen ? "flex" : "hidden"
+    //             } flex-col gap-6 items-start absolute top-20 right-4 bg-[#101010] p-6 rounded-lg shadow-lg md:hidden transition-all duration-300`}
+    //           >
+    //             <li>
+    //               <Link
+    //                 to="/about"
+    //                 className="text-[15px] font-normal font-main text-[#E6E6E6]"
+    //                 onClick={() => setIsOpen(false)}
+    //               >
+    //                 About
+    //               </Link>
+    //             </li>
+    //             <li>
+    //               <Link
+    //                 to="/portfolio"
+    //                 className="text-[15px] font-normal font-main text-[#E6E6E6]"
+    //                 onClick={() => setIsOpen(false)}
+    //               >
+    //                 Portfolio
+    //               </Link>
+    //             </li>
+    //             <li>
+    //               <Link
+    //                 to="/contact"
+    //                 className="text-[15px] font-normal font-main text-[#E6E6E6]"
+    //                 onClick={() => setIsOpen(false)}
+    //               >
+    //                 Contact
+    //               </Link>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 
