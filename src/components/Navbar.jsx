0b322b3fc151949abcc4da8e3f2a535a99b6e2c5
Antor Web-Dev <antorremeni@gmail.com>
2025-10-02 11:36:31 +0600

    chnage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,114 +8,12 @@ const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
 
-jQuery(function ($) {
-	var topMenuHeight = $("#desktop-nav").outerHeight();
-	$("#desktop-nav").menuScroll(topMenuHeight);
-});
-
-// COPY THE FOLLOWING FUNCTION INTO ANY SCRIPTS
-jQuery.fn.extend({
-	menuScroll: function (offset) {
-		// Declare all global variables
-		var topMenu = this;
-		var topOffset = offset ? offset : 0;
-		var menuItems = $(topMenu).find("a");
-		var lastId;
-
-		// Save all menu items into scrollItems array
-		var scrollItems = $(menuItems).map(function () {
-			var item = $($(this).attr("href"));
-			if (item.length) {
-				return item;
-			}
-		});
-
-		// When the menu item is clicked, get the #id from the href value, then scroll to the #id element
-		$(topMenu).on("click", "a", function (e) {
-			var href = $(this).attr("href");
-
-			var offsetTop = href === "#" ? 0 : $(href).offset().top - topOffset;
-
-			$("html, body").stop().animate(
-				{
-					scrollTop: offsetTop
-				},
-				300
-			);
-			e.preventDefault();
-		});
-
-		// When page is scrolled
-		$(window).scroll(function () {
-			var nm = $("html").scrollTop();
-			var nw = $("body").scrollTop();
-			var fromTop = (nm > nw ? nm : nw) + topOffset;
-
-			// When the page pass one #id section, return all passed sections to scrollItems and save them into new array current
-			var current = $(scrollItems).map(function () {
-				if ($(this).offset().top <= fromTop) return this;
-			});
-
-			// Get the most recent passed section from current array
-			current = current[current.length - 1];
-			var id = current && current.length ? current[0].id : "";
-			if (lastId !== id) {
-				lastId = id;
-				// Set/remove active class
-				$(menuItems)
-					.parent()
-					.removeClass("active")
-					.end()
-					.filter("[href='#" + id + "']")
-					.parent()
-					.addClass("active");
-			}
-		});
-	}
-});
 
 
   return (
     <>
-
     
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
-            </div>
-
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-   
-                    <li class="hidden">
-                        <a class="page-scroll" href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">About</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#services">Services</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-
-    </nav>
-
-
-    
-    {/* <nav id="navbar" className="bg-[#101010] sticky top-0 z-50">
+    <nav id="navbar" className="bg-[#101010] sticky top-0 z-50">
       <div className="container">
         <div id="navbar-row" className="flex items-center px-[20px] lg:px-0  justify-between">
         
@@ -213,7 +111,7 @@ jQuery.fn.extend({
           </div>
         </div>
       </div>
-    </nav> */}
+    </nav>
     </>
   );
 };
